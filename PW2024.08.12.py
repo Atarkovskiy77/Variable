81f5acc81766df9d0f3915f1c3aaf53b184b0e79
Atarkovskiy77 <tarcovskiy.77@gmail.com>
2024-08-12 17:43:27 +0300

    Initial commit

---
+++ PW2024.08.12.py
@@ -0,0 +1,7 @@
+first_name = 'Курс:'
+second_name = 'Python,'
+third_name = 'всего задач:'
+four_name = 'затрачено часов:'
+five_name = 'среднее время выполнения'
+six_name = 'часа.'
+print(first_name, second_name, third_name,12, four_name, 1.5, five_name, 0.125, six_name)
---
+++ Variable.py
@@ -0,0 +1,8 @@
+name = 'Andrey'
+print(name)
+dateOfBirth = 'March 1976'
+print(dateOfBirth)
+name = 'Helga'
+print(name)
+date_of_birth = 'October 1982'
+print(date_of_birth)
