55c355c465badc23c72e0b94bdd5ce657741bc5b
Atarkovskiy77 <tarcovskiy.77@gmail.com>
2024-08-13 17:06:28 +0300

    Add files via upload

---
+++ Variable_12.08.2024.py
@@ -0,0 +1,9 @@
+first_name = 'Курс:'
+second_name = 'Python,'
+third_name = 'Всего задач:'
+total_tasks = 12
+four_name = 'затрачено часов:'
+hours_spent = 1.5
+five_name = 'среднее время выполнения'
+six_name = 'часа.'
+print(first_name, second_name, third_name, total_tasks, four_name, hours_spent, five_name, (hours_spent/total_tasks), six_name)
