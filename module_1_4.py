8d2adb18f1c8b8779badf6c46594dd28e87a7c30
Atarkovskiy77 <tarcovskiy.77@gmail.com>
2024-08-27 11:21:27 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,7 +1,7 @@
-my_string = input('Введите название предмета: ')
-print(len(my_string))
-print('Введите название предмета: '.upper())
-print('Введите название предмета: '.upper().lower())
-print('Введите название предмета: '.replace(' ',''))
-print('Первый символ строки: ', my_string[0])
-print('Последний символ строки: ', my_string[-1])
+my_string = str(input('Введите название предмета: '))
+print(len(my_string)) # Вывод количества символов введенного текста
+print(my_string.upper()) # Вывод строки в верхнем регистре
+print(my_string.lower()) # Вывод строки в нижнем регистре
+print(my_string.replace(' ','')) # Вывод строки с удалёнными пробелами
+print(my_string[0]) # Вывод первого символа строки
+print(my_string[-1]) # Вывод последнего символа строки
