d1b9b5663a9811ec597634caa9013d01d35f4a7c
Atarkovskiy77 <tarcovskiy.77@gmail.com>
2024-09-02 10:42:06 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -3,12 +3,11 @@ immutable_var = 1, 5.5, 'Andrey', True, 'gender'
 # Выполнить операцию вывода кортежа immutable_var на экран
 print(immutable_var)
 # 3. Попытаться изменить элементы кортежа immutable_var
-# immutable_var [3] = 100
+# immutable_var[3] = 100 # Кортеж является неизменяемым типом данных.
 # При выводе ответа выдаётся ошибка
 # Ошибка буквально сообщает, что кортеж не поддерживает обращение по элементам
-# То есть кортеж является неизменяемым списком
 # 4. Создать изменяемую структуру данных
-mutable_list =([1, 5.5, 'Andrey', True, 'gender'], 0)
+mutable_list =([1, 2,'Andrey','excellent','guy', True], 0, 5, 9)
 print(mutable_list)
-mutable_list [0][4] = 'male'
+mutable_list [0][4] = 'man'
 print(mutable_list)
