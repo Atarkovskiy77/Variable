8532500fa740b45a6eedfaefe13ad8a921b8471d
Atarkovskiy77 <tarcovskiy.77@gmail.com>
2024-09-19 21:51:49 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,10 @@
+# Записываем переменную
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+# Пишем цикл с условиями
+while index < len(my_list):
+    if my_list[index] < 0:
+        break # используем оператор прерывания
+    if my_list[index] > 0:
+        print(my_list[index])
+    index += 1
